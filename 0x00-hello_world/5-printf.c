73ebe4af7750cd214be0f70b4797e3e322fad476
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-07 07:34:38 -0700

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that print with printf function
+ * Return: Always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
