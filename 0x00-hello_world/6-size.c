ced79a16e41bdff37a6911ad2ba73c2df8545748
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-07 07:57:15 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	printf("char size %lu byte(s)\n", sizeof(char));
-	printf("int size %lu byte(s)\n", sizeof(int));
-	printf("long int size %lu byte(s)\n", sizeof(long int));
-	printf("long long size %lu byte(s)\n", sizeof(long long));
-	printf("float size %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
