d008996cce4c2408b7470eddeffb46aa233670db
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-08 08:49:06 -0700

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,11 @@ int main(void)
 	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 	{
 		putchar(alphabet);
-		for (alphabet = 'a' ; alphabet >= 'z' ; alphabet++)
-		{
-			putchar(alphabet);
-		}
 	}
+	for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
+        {
+                putchar(alphabet);
+        }
 	putchar('\n');
 	return (0);
 }
