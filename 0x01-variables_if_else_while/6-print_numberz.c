59a43b571f9942f6ccca5ca311561423f8c029ee
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-08 10:33:43 -0700

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that print with printf function
+ * Return: Always 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
