faedf01a3a62b918421f319f8f536493c09d9320
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-08 08:42:11 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that print with printf function
+ * Return: Always 0
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
