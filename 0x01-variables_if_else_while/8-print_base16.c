a6db65aabb88063e24cde7ae01fe97637f52c614
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-08 11:18:02 -0700

    .

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,15 +4,20 @@
  * Description: A C program that print with printf function
  * Return: Always 0
 */
-int main() {
-    int i;
-    for (i = 0; i < 16; i++) {
-        if (i < 10) {
-            putchar("%d", i);
-        } else {
-            putchar("%c", 'a' + i - 10);
-        }
-    }
-    putchar("\n");
-    return 0;
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		} else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
