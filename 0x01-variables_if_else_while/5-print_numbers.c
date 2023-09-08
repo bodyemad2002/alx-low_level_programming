ae519775ec0b136b1c44358bde45d78237bae64f
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-08 10:16:30 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that print with printf function
+ * Return: Always 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%ddigit numbers: ", n);
+	return (0);
+}
