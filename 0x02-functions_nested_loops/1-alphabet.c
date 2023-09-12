e73de931c4105fd0b09e2e188341c103d2550041
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 08:02:11 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,14 +14,3 @@ void print_alphabet(void)
 	}
 	putchar('\n');
 }
-/**
- * main - Entry point
- * Description: A C program that print _putchar
- * Return: Always 0
-*/
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
