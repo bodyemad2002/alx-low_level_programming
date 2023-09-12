8feb06586f8afa8c92d4e96b544d8c287cec7562
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 10:53:15 -0700

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 /**
   * print_sign - Checks for lowercase character
-  * @c: The character to be checked
-  *
-  * Return: 1 if n is greater than zero or 0 if n is zero and -1 if n is less than zero
+  * @n: The character to be checked
+  * Return: 1 if + or 0 if 0 and -1 if -
   */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
