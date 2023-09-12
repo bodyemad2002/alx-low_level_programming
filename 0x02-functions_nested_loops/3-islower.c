4f552ea57bec1064e95b908ca3dbd8da35c44650
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 10:17:53 -0700

    .

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - Entry point
+ * Description: A C program that print _putchar
+ * Return: void
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+
+}
