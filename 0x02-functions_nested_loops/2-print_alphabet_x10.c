dcbb6778137da8cb2128b6a11d15ad8bddcb1ce5
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 09:11:40 -0700

    .

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point
+ * Description: A C program that print _putchar
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int x,y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
