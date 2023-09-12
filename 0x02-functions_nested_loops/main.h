3bb360d2ee7c101ca471620c995f6fe675728ab7
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 08:12:33 -0700

    .

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: A C program that print with printf function
+ * Return: Always 0
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
