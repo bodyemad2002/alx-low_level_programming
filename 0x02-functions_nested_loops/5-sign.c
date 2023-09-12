d6082a61dd1bd40fade0052d45baeb19284dbec8
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 10:45:45 -0700

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,19 @@
 int print_sign(int n)
 {
 	if(n > 0)
+	{
 		printf("+");
-	return (1);
-	if(n < 0)
+		return (1);
+	}
+	else if(n < 0)
+	{
 		printf("-");
-	return (-1);
-	if(n = 0)
+		return (-1);
+	}
+	else
+	{
 		printf("0");
-	return (0);
+		return (0);
+	}
 
 }
