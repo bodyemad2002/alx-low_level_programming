d9d1952e778935030459926a0fccd6223679a2ad
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 10:32:18 -0700

    .

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * _islower - Checks for  character
+  * _isalpha - Checks for  character
   * @c: The character to be checked
   *
   * Return: 1 for lowercase character or 0 for anything else
   */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
                 return (1);
