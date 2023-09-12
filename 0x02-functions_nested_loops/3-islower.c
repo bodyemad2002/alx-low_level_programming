6fd0fc0bc3bb5ac337f8f474de8a6e611b47e8bb
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-12 10:27:50 -0700

    .

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _islower - Entry point
- * Description: A C program that print _putchar
- * Return: void
-*/
+  * _islower - Checks for lowercase character
+  * @c: The character to be checked
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
