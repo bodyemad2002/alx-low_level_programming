c1bc8a9bd965902fc1bcdaceb827138c9b0c878b
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 06:10:32 -0700

    .

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * times_table - prints the 9 times table, starting with 0
+  * @void: The character to be checked
+  * Return: Always 0
+  */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			printf(x, y);
+		}
+	}
+}
