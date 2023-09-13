0fed1c43b043a2f6a4b81505e4b83750832e8cd8
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 09:34:06 -0700

    .

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that print _putchar
+ * Return: Always 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 50; x++)
+	{
+		printf("%d, ", x);
+	}
+	printf('\n');
+	return (0);
+}
