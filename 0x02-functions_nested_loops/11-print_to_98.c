cb861c2e9f219d8d6533c546405d26c55c0e27c7
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 06:42:23 -0700

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_to_98 - prints every minute of the day of Jack Bauer
+  * @n: The character to be checked
+  * Return: Always 0
+  */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n);
+	}
+}
