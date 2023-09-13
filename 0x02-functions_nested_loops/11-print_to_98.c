9a7887c6ca65fda140e8e526947b0e1df7e68d5b
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 09:16:11 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,9 +7,9 @@
   */
 void print_to_98(int n)
 {
-	if (n < 99)
+	if (n >= 98)
 	{
-		for (n = 0; n < 98; n++)
+		for (n = 0; n > 98; n--)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -18,7 +18,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n = 0; n > 98; n--)
+		for (n = 0; n < 98; n++)
 		{
 			_putchar(',');
 			_putchar(' ');
