2c16a56c86ce2287892fd0a4eda491aa8cb6ed22
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 05:47:30 -0700

    .

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * jack_bauer - prints every minute of the day of Jack Bauer
+  * @void: The character to be checked
+  * Return: Always 0
+  */
+void jack_bauer(void)
+{
+	int x;
+
+	for (x = 00 : 00; x < 24 : 00; x++)
+		printf(x);
+	return (x);
+}
