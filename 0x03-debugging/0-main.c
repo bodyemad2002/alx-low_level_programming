424d7696c906b6542c2a49e64c80a4e63d49dce2
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 11:33:15 -0700

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that print _putchar
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
