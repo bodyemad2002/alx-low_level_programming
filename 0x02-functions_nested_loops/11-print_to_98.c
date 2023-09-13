3141387467ffac75289656d2397eea846692619e
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 09:12:51 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * print_to_98 - prints every minute of the day of Jack Bauer
+  * print_to_98 - prints all natural numbers from n to 98
   * @n: The character to be checked
   * Return: Always 0
   */
 void print_to_98(int n)
 {
-	for (n = 0; n < 99; n++)
+	if (n < 99)
+	{
+		for (n = 0; n < 99; n++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(n);
+		}
+	}
+	else
 	{
 		for (n = 0; n > 99; n--)
 		{
