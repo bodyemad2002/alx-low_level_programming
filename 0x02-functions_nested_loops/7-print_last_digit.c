7ed78813ab918e47949e5b2f184aa21f4c197962
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 05:27:26 -0700

    .

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_last_digit - prints the last digit of a number
+  * @n: The character to be checked
+  * Return: Always 0
+  */
+int print_last_digit(int n)
+{
+	last_digit = n % 10;
+	printf(last_digit);
+}
