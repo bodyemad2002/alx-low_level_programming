a6a1876ff5356929747d6d5d46050ed2c9dbf69b
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 05:10:52 -0700

    .

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 /**
   * _abs - computes the absolute value
-  * @r: The character to be checked
+  * @int: The character to be checked
   * Return: Always 0
   */
 int _abs(int)
 {
-	if (r < 0)
-		return (-r);
+	if (int < 0)
+		return (-int);
 	else
-		return (r);
+		return (int);
 }
