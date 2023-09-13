61d162fe230a1a027831ad77090a66fd9613d0a8
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 06:27:10 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,26 @@
   */
 void times_table(void)
 {
-	int x;
+	int num, mult, prod;
 
-	for (x = 0; x < 10; x++)
+	for (num = 0; num < 10; num++)
 	{
-		printf("%d * 9 = %d\n", x, x*9);
+		_putchar('0');
+
+		for (mult = 1; mult < 10; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod < 10)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
