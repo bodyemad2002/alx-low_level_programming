2d84ce81cab59d4b9eb303384c7d6945d8ab9692
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-13 06:33:16 -0700

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * add - prints every minute of the day of Jack Bauer
+  * @x: The character to be checked
+  * @y: The character to be checked
+  * Return: Always 0
+  */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	_putchar(z);
+	return (z);
+}
