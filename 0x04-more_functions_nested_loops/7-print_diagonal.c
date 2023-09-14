8816fd51469b20ea85a9be44757a9bc80c4bea02
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-14 09:05:03 -0700

    .

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_diagonal -  checks for a digit
+  * @n: The character to be checked
+  *
+  * Return: Always 0
+  */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+
+		for (x = 1; x <= n; x++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
