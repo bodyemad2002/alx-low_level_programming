146dbe57c87bf438f95ab2d0b314302c47e42ba1
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-14 07:14:32 -0700

    .

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * mul - Checks for uppercase character
+  * @a: The character to be checked
+  * @b: The character to be checked
+  *
+  * Return: Always 0
+  */
+int mul(int a, int b)
+{
+	printf("%d\n", mul(a, b));
+	return (0);
+}
