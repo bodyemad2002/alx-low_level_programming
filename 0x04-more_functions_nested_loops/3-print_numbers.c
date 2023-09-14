d44b6eccbe45c6960fcf8295d737d11e5eec990b
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-14 07:22:49 -0700

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_numbers -  checks for a digit
+  * @void: The character to be checked
+  *
+  * Return: Always 0.
+  */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
