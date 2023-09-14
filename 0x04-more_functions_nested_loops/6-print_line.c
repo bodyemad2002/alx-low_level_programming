dd177fe11a306766d507c586fa3b768a37e5547c
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-14 08:58:28 -0700

    .

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -17,10 +17,10 @@ void print_line(int n)
 	{
 		int x;
 
-        for (x = 1; x < n; x++)
-	{
-		_putchar("_");
-	}
-        _putchar('\n');
+		for (x = 1; x <= n; x++)
+		{
+			_putchar("_");
+		}
+		_putchar('\n');
 	}
 }
