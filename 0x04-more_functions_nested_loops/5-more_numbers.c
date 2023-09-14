358385de04fb83c6e29e817351ff1a0230064dfe
bodyemad2002 <142815159+bodyemad2002@users.noreply.github.com>
2023-09-14 18:44:31 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,24 @@
   *
   * Return: Always 0
   */
-void more_numbers(void);
+void more_numbers(void)
 {
-	int x, y;
 
-	for (x = 0; x <= 9; x++)
+	int a = 0;
+	int b = 0;
+
+	while (b <= 9)
 	{
-		for (y = 0; y <= 14; y++)
+		while (a <= 14)
 		{
-			if (y > 9)
-				_putchar (y / 10 + '0');
-			_putchar (y % 10 + '0');
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			a++;
+
 		}
-	}
 	_putchar('\n');
+	b++;
+	a = 0;
+	}
 }
