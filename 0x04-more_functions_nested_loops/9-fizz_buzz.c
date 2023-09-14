769080ae3758123c1d7ff2e85ed180763ff28b12
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-14 09:37:38 -0700

    .

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Descripation: prints the numbers from 1 to 100
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz%c");
+
+			else if (i % 5 == 0)
+			{
+				printf("Buzz%c");
+
+				else if (i % 3 == 0 && i % 5 == 0)
+				{
+					printf("FizzBuzz%c");
+				}
+					printf("%d", i);
+			}
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
