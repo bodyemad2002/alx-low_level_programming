6aab7898f28f22b4d09fcb712e9d35ee8c5163d8
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-14 07:56:53 -0700

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void);
 {
 	int x, y;
 
-	for (x = '0'; x <= '14'; x++)
+	for (x = '1'; x <= "10"; x++)
 	{
-		for (y = '0'; y <= '10'; y++)
+		for (y = '0'; y <= "14"; y++)
 		{
 			_putchar(x);
 		}
