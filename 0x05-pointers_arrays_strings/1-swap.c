ecdb21eb322f701d66f6cc1bee621badcfd3301f
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-18 12:39:22 -0700

    .

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,10 +9,9 @@
  */
 void swap_int(int *a, int *b)
 {
-	*a = 98;
-	*b = 42;
-	scanf("%d%d", &a, &b);
-	swap(a, b);
-	printf("%d", a);
-	printf("%d", b);
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
 }
