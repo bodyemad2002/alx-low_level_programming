bdfdf5c3639dba3d6bcb3c9ec73cac7335126700
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-18 12:26:21 -0700

    .

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: The character to be checked
+ * @b: The character to be checked
+ * Return: Always 0.
+ */
+void void swap_int(int *a, int *b)
+{
+	&a = 98;
+	&b = 42;
+}
