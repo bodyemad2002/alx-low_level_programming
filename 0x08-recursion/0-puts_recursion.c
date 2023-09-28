08122165353fcae837cfc81788238cf249f0abd2
bodyemad2002 <bodyemad123456@gmail.com>
2023-09-28 05:39:30 -0700

    .

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * _puts_recursion - check the code
- * @*s: The character to be checked
+ * @s: The character to be checked
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
 	puts("Puts with recursion");
-	return (0);
 }
